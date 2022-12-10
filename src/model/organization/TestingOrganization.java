bb9819a01bc9ff1c76f8cc93b223b0a3fe377a36
Gayatri Rajendra Dalvi <dalvi.ga@northeastern.edu>
2022-12-10 18:30:21 -0500

    Added Health care and testing organization

---
+++ src/model/organization/TestingOrganization.java
@@ -0,0 +1,20 @@
+package model.organization;
+import java.util.ArrayList;
+import model.role.TestingLabAssistantRole;
+import model.role.Role;
+
+public class TestingOrganization extends Organization{
+
+    public TestingOrganization() {
+        super(Organization.Type.Lab.getValue());
+    }
+    @Override
+    public ArrayList<Role> getValidRole() {
+        ArrayList<Role> roles = new ArrayList();
+        roles.add(new TestingLabAssistantRole());
+        return roles;
+    }
+
+    
+    
+}
